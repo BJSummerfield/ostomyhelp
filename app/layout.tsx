08cd566e442775962bb43b5d80a81c64a536113b
brian <brianjsummerfield@gmail.com>
2023-07-20 16:28:55 -0500

    update tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Ostomy Help Now',
+  title: 'Find My Nurse',
   description: 'Helping Chicagoland',
 }
 
